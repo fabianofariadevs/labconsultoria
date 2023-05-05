2d98e6bc42734a773215b5e0f9d54a41d82c404e
fabianofariadevs <fabianofariadevs2022@gmail.com>
2023-05-05 02:15:54 -0300

    add ajustes_form2

--- sistema/Controlador/Admin/AdminReceitas.php
+++ sistema/Controlador/Admin/AdminReceitas.php
@@ -3,27 +3,72 @@
 namespace sistema\Controlador\Admin;
 
 use sistema\Modelo\ReceitaModelo;
-use sistema\Nucleo\helpers;
+use sistema\Nucleo\Helpers;
 
+/**
+ * Classe AdminReceitas
+ *
+ * @author Fabiano Faria
+ */
 class AdminReceitas extends AdminControlador
 {
+
+    /**
+     * Lista Receitas
+     * @return void
+     */
     public function listar(): void
     {
+        $receita = new ReceitaModelo();
+
         echo $this->template->renderizar('receitas/listar.html', [
-            'receitas'=> (new ReceitaModelo())->busca()
+            'receitas' => $receita->busca()->ordem('nome_receita ASC')->resultado(true),
+            'total' => [
+                'receitas' => $receita->total(),
+                'receitasAtiva' => $receita->busca('status = 1')->total(),
+                'receitasInativa' => $receita->busca('status = 0')->total(),
+            ]
         ]);
     }
-    
+
+    /**
+     * Cadastra Receita
+     * @return void
+     */
     public function cadastrar(): void
     {
         $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
-        //var_dump($dados);
         if (isset($dados)) {
-            (new ReceitaModelo())->armazenar($dados);     
-            helpers::redirecionar('admin/receitas/listar');
+
+            if ($this->validarDados($dados)) {
+                $receita = new ReceitaModelo();
+
+                $receita->usuario_id = $this->usuario->id;
+                $receita->nome_receita = $dados['nome_receita'];
+                $receita->descricao_receita = $dados['descricao_receita'];
+                $receita->modo_preparo = $dados['modo_preparo'];
+                $receita->qtde_prevista_receita = $dados['qtde_prevista_receita'];
+                $receita->validade_receita = $dados['validade_receita'];
+                $receita->observacao_receita = $dados['observacao_receita'];
+                $receita->status = $dados['status'];
+                $receita->id_tbl_cliente_fabrica = $dados['id_tbl_cliente_fabrica'];
+                $receita->tbl_produto_mix_id_tbl_produto_mix = $dados['tbl_produto_mix_id_tbl_produto_mix'];
+                $receita->tbl_produto_mix_cod_prod_mix = $dados['tbl_produto_mix_cod_prod_mix'];
+                $receita->slug = Helpers::slug($dados['nome_receita']);
+
+                if ($receita->salvar()) {
+                    $this->mensagem->sucesso('Receita cadastrada com sucesso')->flash();
+                    Helpers::redirecionar('admin/receitas/listar');
+                } else {
+                    $this->mensagem->erro($receita->erro())->flash();
+                    Helpers::redirecionar('admin/receitas/listar');
+                }
+            }
         }
-        
-        echo $this->template->renderizar('receitas/formulario.html', []);
+
+        echo $this->template->renderizar('receitas/formulario.html', [
+            'receitas' => (new ReceitaModelo())->busca()
+        ]);
     }
 
 }
